ef629962bb9aa30d0e2fcc504654e4400b4dcbf0
Romain Rochegude <romain.rochegude@gmail.com>
2016-06-01 20:26:45 +0200

    config

--- NativeScriptStarter/platforms/ios/build/NativeScriptStarter.build/Debug-iphonesimulator/NativeScriptStarter.build/Objects-normal/i386/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/main.m \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/Romain/GitHub/NativeScriptStarter/NativeScriptStarter/platforms/ios/internal/TNSDebugging.h
